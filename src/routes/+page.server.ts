9221ff00f9187510c5c2e1c1a54a80421418e49a
Mateo Roldos <mateoroldos19@gmail.com>
2026-06-29 19:37:22 -0300

    feat: add cloudflare adapter

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,8 @@
 import figlet from 'figlet';
+import Pagga from 'figlet/importable-fonts/Pagga.js';
+
+figlet.parseFont('Pagga', Pagga);
 
 export const load = () => ({
-	ascii: figlet.textSync('goodlab', {
-		font: 'Pagga'
-	})
+	ascii: figlet.textSync('goodlab', { font: 'Pagga' })
 });
